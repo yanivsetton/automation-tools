8cd3a488cc5cd82959ff9d0ece5111be13ae76c5
Yaniv <yanivsiton89@gmail.com>
2023-07-19 22:22:10 +0300

    Removed support for license file activation

--- Install_inteliji.ps1
+++ Install_inteliji.ps1
@@ -2,13 +2,9 @@
 $installerUrl = 'https://download.jetbrains.com/idea/ideaIU-2023.1.4.exe?_gl=1*vmm28x*_ga*MTczMTY4NDk1OS4xNjg5NzcxMjMx*_ga_9J976DJZ68*MTY4OTc4NzAyMy40LjAuMTY4OTc4NzAyNC4wLjAuMA..&_ga=2.32101822.1293864214.1689771231-1731684959.1689771231'
 $installerExe = 'IntelliJ-Ultimate-Installer.exe'
 $installPath = 'C:\Program Files\JetBrains\IntelliJ IDEA Ultimate Edition'
-$licenseFile = 'C:\Path\To\Your\License\idea.license'
 
 # Download the installer
 Invoke-WebRequest -Uri $installerUrl -OutFile $installerExe
-
-# Copy the license file to the installation directory
-Copy-Item -Path $licenseFile -Destination $installPath -Force
 
 # Install IntelliJ IDEA Ultimate silently
 Start-Process -FilePath $installerExe -ArgumentList "/S", "/D=$installPath" -Wait
