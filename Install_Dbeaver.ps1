d4f3538e9d538ff1ffdb78fe17f31a0b515e40f6
Yaniv <yanivsiton89@gmail.com>
2023-07-27 17:55:38 +0300

    Update Install_Dbeaver.ps1

--- Install_Dbeaver.ps1
+++ Install_Dbeaver.ps1
@@ -1,4 +1,11 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Install DBeaver Community Edition silently
-choco install dbeaver -y --params "/S"
+# Install DBeaver using Chocolatey
+$packageName = 'dbeaver'
+$installArgs = '/S'
+$chocoInstallScript = "choco install $packageName $installArgs"
+
+# Run the installation command
+Invoke-Expression $chocoInstallScript
+#choco install dbeaver -y --params "/S"
 
