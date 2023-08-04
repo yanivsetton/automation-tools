50b07a2ace473be990aaa603614c1dc02e7d8efe
Yaniv <yanivsiton89@gmail.com>
2023-08-04 17:22:21 +0300

    Update Install_inteliji.ps1

--- scripts/Install_inteliji.ps1
+++ scripts/Install_inteliji.ps1
@@ -1,19 +1,4 @@
-# Define variables
-#$installerUrl = 'https://download.jetbrains.com/idea/ideaIU-2023.1.4.exe?_gl=1*vmm28x*_ga*MTczMTY4NDk1OS4xNjg5NzcxMjMx*_ga_9J976DJZ68*MTY4OTc4NzAyMy40LjAuMTY4OTc4NzAyNC4wLjAuMA..&_ga=2.32101822.1293864214.1689771231-1731684959.1689771231'
-#$installerExe = 'IntelliJ-Ultimate-Installer.exe'
-#$installPath = 'C:\Program Files\JetBrains\IntelliJ IDEA Ultimate Edition'
-
-# Download the installer
-#Invoke-WebRequest -Uri $installerUrl -OutFile $installerExe
-
-# Install IntelliJ IDEA Ultimate silently
-#Start-Process -FilePath $installerExe -ArgumentList "/S", "/D=$installPath" -Wait
-
-# Remove the installer file
-#Remove-Item -Path $installerExe -Force
-# Check if Chocolatey is installed
-# Check if Chocolatey is installed
-# Check if Chocolatey is installed
+# Check if Chocolatey is already installed
 if (!(Test-Path "$env:ProgramData\chocolatey\choco.exe")) {
     Write-Host "Chocolatey is not installed. Installing Chocolatey..."
     Set-ExecutionPolicy Bypass -Scope Process -Force
@@ -21,16 +6,25 @@ if (!(Test-Path "$env:ProgramData\chocolatey\choco.exe")) {
 }
 
 # Define installation path (change it to your desired location)
-$installationPath = "C:\IntelliJIDEA"
+$installationPath = "C:\Program Files\JetBrains\IntelliJ IDEA Ultimate Edition"
 
-# Install IntelliJ IDEA Ultimate with Chocolatey (silent with default options)
-Write-Host "Installing IntelliJ IDEA Ultimate..."
-choco install intellijidea-ultimate -y --force --ia "/S /ALLUSERS=1 /D=$installationPath"
+# Define the package name
+$packageName = 'intellijidea-ultimate'
 
-# Check if the installation was successful
-if (!(Test-Path "$installationPath")) {
-    Write-Host "IntelliJ IDEA Ultimate installation failed. Please check the logs for any errors."
-}
-else {
-    Write-Host "IntelliJ IDEA Ultimate installed successfully!"
+# Check if IntelliJ IDEA Ultimate is already installed
+$checkInstall = choco list --local-only $packageName
+
+if ($checkInstall -like "*$packageName*") {
+    Write-Host "IntelliJ IDEA Ultimate is already installed."
+} else {
+    # Install IntelliJ IDEA Ultimate with Chocolatey (silent with default options)
+    Write-Host "Installing IntelliJ IDEA Ultimate..."
+    choco install $packageName -y --ia "/S /D=$installationPath"
+    
+    # Check if the installation was successful
+    if (!(Test-Path "$installationPath")) {
+        Write-Host "IntelliJ IDEA Ultimate installation failed. Please check the logs for any errors."
+    } else {
+        Write-Host "IntelliJ IDEA Ultimate installed successfully!"
+    }
 }
