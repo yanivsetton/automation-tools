2a2b44e27b98091aedce6f116da3c3818348feab
Yaniv <yanivsiton89@gmail.com>
2023-08-04 17:13:04 +0300

    Update Install_Dbeaver.ps1

--- scripts/Install_Dbeaver.ps1
+++ scripts/Install_Dbeaver.ps1
@@ -1,11 +1,24 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# Install DBeaver using Chocolatey (latest version, silent installation)
+# Check if Chocolatey is already installed
+if (!(Test-Path -path "$env:ProgramData\chocolatey")) {
+    # Install Chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# Package name to check
 $packageName = 'dbeaver'
 
-# Install DBeaver using Chocolatey with the latest version and silent switch
-$chocoInstallScript = "choco install $packageName --yes --params '/S'"
+# Check if DBeaver is already installed
+$checkInstall = choco list --local-only $packageName
 
-# Run the installation command
-Invoke-Expression $chocoInstallScript
-#choco install dbeaver -y --params "/S"
+if ($checkInstall -like "*$packageName*") {
+    Write-Host "DBeaver is already installed."
+} else {
+    # Install DBeaver using Chocolatey with the latest version and silent switch
+    $chocoInstallScript = "choco install $packageName --yes --params '/S'"
 
+    # Run the installation command
+    Invoke-Expression $chocoInstallScript
+    Write-Host "DBeaver has been installed."
+}
