a29ef4cf06512f4ccfb9b3c697af4cdec958fc00
Yaniv <yanivsiton89@gmail.com>
2023-08-06 11:49:03 +0300

    Update dbeaver-new-connection.ps1

--- scripts/dbeaver-new-connection.ps1
+++ scripts/dbeaver-new-connection.ps1
@@ -1,22 +1,6 @@
- #Setting Variables
-#$DBName = Read-Host -Prompt 'Input your server  name'
-#$DBHost = Read-Host -Prompt 'Input your server  url'
-#$DBUser = Read-Host -Prompt 'Input your user name'
-#$DBPass = Read-Host -Prompt 'Input your user password'
-$DBName = $args[0]
-$DBHost = $args[1]
-$DBPass = $args[2]
-#Dbeaver-cli path
-$dbeaverCliPath = "C:\Program Files\DBeaver\dbeaver-cli.exe"
-
-# Start-Process -FilePath $dbeaverCliPath "-con name=$DBName|driver=mysql|host=$DBHost|port=3306|user=$env:USERNAME|password=$DBPass" 
-
  param (
-    [string]$newHost,
-    [string]$user,
-    [string]$password,
-    [string]$port,
-    [string]$database
+    [string]$newUser,
+    [string]$newPassword
 )
 
 # Path to the JSON file
@@ -26,16 +10,13 @@ $jsonFile = "C:\Users\Administrator\Documents\automation-tools\scripts\dbeaver-j
 $jsonString = Get-Content -Path $jsonFile -Raw
 
 # Replace the variables directly in the JSON string using regular expressions
-$jsonString = $jsonString -replace '(?<="host": ")[^"]+', $newHost
-$jsonString = $jsonString -replace '(?<="user": ")[^"]+', $user
-$jsonString = $jsonString -replace '(?<="password": ")[^"]+', $password
-$jsonString = $jsonString -replace '(?<="port": ")[^"]+', $port
-$jsonString = $jsonString -replace '(?<="database": ")[^"]+', $database
+$jsonString = $jsonString -replace '(?<="user": ")[^"]+', $newUser
+$jsonString = $jsonString -replace '(?<="password": ")[^"]+', $newPassword
 
 # Write the updated JSON string back to the same file
 $jsonString | Set-Content -Path $jsonFile
 
-Write-Output "JSON file has been updated with new connection values."
+Write-Output "JSON file has been updated with new username and password."
 
 # Destination path for the copied JSON file
 $destinationPath = "C:\Users\Administrator\AppData\Roaming\DBeaverData\workspace6\General\.dbeaver\data-sources-2.json"
@@ -48,6 +29,3 @@ New-Item -ItemType Directory -Force -Path $destinationDirectory | Out-Null
 Copy-Item -Path $jsonFile -Destination $destinationPath -Force
 
 Write-Output "Updated JSON file has been copied to $destinationPath."
- 
-powershell.exe -ExecutionPolicy Bypass -File DBeaver-connections.ps1 -newHost "another-test" -user "$env:USERNAME" -password "new_password" -port "3306" -database "new_database-again" 
-
